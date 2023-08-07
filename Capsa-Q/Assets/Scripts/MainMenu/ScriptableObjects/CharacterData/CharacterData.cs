76b98483179a2099a7d11c451c78b0bd1a055c7b
Bawenang R. P. P <bawenang@gmail.com>
2023-08-07 16:19:17 +0700

    Add CharacterData ScriptableObject and its data

---
+++ Capsa-Q/Assets/Scripts/MainMenu/ScriptableObjects/CharacterData/CharacterData.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+[CreateAssetMenu(fileName = "CharacterData", menuName = "ScriptableObjects/CharacterData", order = 1)]
+public class CharacterData : ScriptableObject
+{
+    public enum SpriteState {
+        Idle,
+        Play,
+        Win,
+        Lose
+    }
+    public string charName;
+
+    public string GetSpriteName(SpriteState state) {
+        switch (state)
+        {
+            case SpriteState.Idle: return charName + "_idle";
+            case SpriteState.Play: return charName + "_play";
+            case SpriteState.Win: return charName + "_win";
+            case SpriteState.Lose: return charName + "_lose";
+            default: return "";
+        }
+    }
+
+    public Sprite GetSprite(SpriteState state) {
+        var spriteName = GetSpriteName(state);
+        return AtlasSpriteStore.Instance.GetSprite(spriteName);
+    }
+}
