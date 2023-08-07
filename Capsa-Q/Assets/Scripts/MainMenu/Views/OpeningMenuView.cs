f1800750c3a354d87e930412cdcf39bb8cda0b94
Bawenang R. P. P <bawenang@gmail.com>
2023-08-07 10:14:32 +0700

    Rename Context folder to Scripts

---
+++ Capsa-Q/Assets/Scripts/MainMenu/Views/OpeningMenuView.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpeningMenuView : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+
+    void Awake() 
+    {
+        playButton.onClick += 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnPlayButton() 
+    {
+        
+    }
+}
