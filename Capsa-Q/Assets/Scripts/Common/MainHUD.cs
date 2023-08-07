8a57a8873e7183d4ec9b9a80e3888c8bf5c0903c
Bawenang R. P. P <bawenang@gmail.com>
2023-08-07 11:34:35 +0700

    Refactor MainHUD

--- Capsa-Q/Assets/Scripts/Common/MainHUD.cs
+++ Capsa-Q/Assets/Scripts/Common/MainHUD.cs
@@ -9,19 +9,21 @@ public class MainHUD : MonoBehaviour
     [SerializeField] private Animator fadingImageAnimator;
 
     public delegate void OnFinishFadingIntoScene();
-    public event OnFinishFadingIntoScene onFinishFadingIntoScene;
-
     public delegate void OnFinishFadingOutOfScene();
-    public event OnFinishFadingOutOfScene onFinishFadingOutOfScene;
 
-    public void FadeIntoScene() {
+    private event OnFinishFadingIntoScene onFinishFadingIntoScene;
+    private event OnFinishFadingOutOfScene onFinishFadingOutOfScene;
+
+    public void FadeIntoScene(OnFinishFadingIntoScene completion = null) {
+        onFinishFadingIntoScene = completion;
         fadingImage.color = Color.black;
         fadingImage.gameObject.SetActive(true);
         fadingImageAnimator.Play("FadingIntoScene");
         StartCoroutine(FadeIntoSceneFinished());
     }
 
-    public void FadeOutOfScene() {
+    public void FadeOutOfScene(OnFinishFadingOutOfScene completion = null) {
+        onFinishFadingOutOfScene = completion;
         fadingImage.color = Color.clear;
         fadingImage.gameObject.SetActive(true);
         fadingImageAnimator.Play("FadingOutOfScene");
@@ -31,11 +33,17 @@ public class MainHUD : MonoBehaviour
     private IEnumerator FadeIntoSceneFinished() {
         yield return new WaitForSeconds(1.2f);
         fadingImageAnimator.gameObject.SetActive(false);
-        if (onFinishFadingIntoScene != null) onFinishFadingIntoScene();
+        if (onFinishFadingIntoScene != null) {
+            onFinishFadingIntoScene();
+            onFinishFadingIntoScene = null;
+        }
     }
 
     private IEnumerator FadeOutOfSceneFinished() {
         yield return new WaitForSeconds(1.2f);
-        if (onFinishFadingOutOfScene != null) onFinishFadingOutOfScene();
+        if (onFinishFadingOutOfScene != null) {
+            onFinishFadingOutOfScene();
+            onFinishFadingOutOfScene = null;
+        }
     }
 }
