079c603f5409f97e76f74e9aa451b7eb43ab9640
Bawenang R. P. P <bawenang@gmail.com>
2023-08-07 14:03:37 +0700

    Implement CharacterButton

---
+++ Capsa-Q/Assets/Scripts/MainMenu/Views/CharacterButton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterButton : MonoBehaviour
+{
+    [SerializeField] private Toggle toggle;
+    [SerializeField] private Image photoImage;
+    [SerializeField] private TMPro.TextMeshProUGUI nameText;
+    private int index = -1;
+
+    public delegate void OnSelected(int index);
+    public event OnSelected onSelected;
+
+    public void Populate(Sprite photo, string name, int index) {
+        this.photoImage.sprite = photo;
+        this.nameText.text = name;
+        this.index = index;
+    }
+
+    public void Select() {
+        toggle.isOn = true;
+    }
+    private void Awake() 
+    {
+        toggle.onValueChanged.AddListener(ValueChanged);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ValueChanged(bool isOn) {
+        if(isOn && onSelected != null) onSelected(this.index);
+    }
+}
--- Capsa-Q/Assets/Scripts/MainMenu/Views/CharacterSelectionView.cs
+++ Capsa-Q/Assets/Scripts/MainMenu/Views/CharacterSelectionView.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelectionView : MonoBehaviour
 {
+    [SerializeField] private Button beginButton;
+    [SerializeField] private CharacterButton[] charButtons;
+
+    private int selectedCharId = 0;
+
+    public delegate void OnBeginButton();
+    public event OnBeginButton onBeginButton;
+
+    void OnEnable()
+    {
+        for(int i = 0; i < charButtons.Length; i++) {
+            charButtons[i].onSelected += CharSelected;
+        }
+    }
+
+    void OnDisable()
+    {
+        for(int i = 0; i < charButtons.Length; i++) {
+            charButtons[i].onSelected -= CharSelected;
+        }
+    }
+
+    void Awake()
+    {
+
+        for(int i = 0; i < charButtons.Length; i++) {
+            charButtons[i].Populate(null, "NAME", i);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        charButtons[0].Select();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void BeginGame() {
+        if(onBeginButton != null) onBeginButton();
+    }
+
+    private void CharSelected(int index) 
+    {
+        Debug.Log(">>>>> Selected index = " + index);
+        selectedCharId = index;
     }
 }
