7c797163661bda87cc8d6fc7ee3cff9e751b0e15
Bawenang R. P. P <bawenang@gmail.com>
2023-08-09 22:59:01 +0700

    Add Card prefab with CardInGame script in it

---
+++ Capsa-Q/Assets/Scripts/Game/Views/CardInGame.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class CardInGame : MonoBehaviour, IPointerClickHandler
+{
+    private const int backface = -1;
+    public delegate void OnTap(int value);
+    public event OnTap onTap;
+
+    public bool IsShowing {
+        get => isShowing;
+        set {
+            isShowing = value;
+            if(value) {
+                spriteRenderer.sprite = CardSpriteStore.Instance.GetSprite(cardValue);
+            } else {
+                spriteRenderer.sprite = CardSpriteStore.Instance.GetSprite(backface);
+            }
+        }
+    }
+
+    private SpriteRenderer spriteRenderer;
+    private int cardValue;
+    private bool isShowing;
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Load(int cardValue, bool isShowing)
+    {
+        this.cardValue = cardValue;
+        IsShowing = isShowing;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(onTap != null) onTap(cardValue);
+    }
+}
