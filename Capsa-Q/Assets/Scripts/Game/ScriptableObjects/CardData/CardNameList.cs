547b1d334119fb58f578259160184e668f1bdc3f
Bawenang R. P. P <bawenang@gmail.com>
2023-08-09 22:14:52 +0700

    Add CardNameList scriptable object to handle the names of card textures.

---
+++ Capsa-Q/Assets/Scripts/Game/ScriptableObjects/CardData/CardNameList.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+[CreateAssetMenu(fileName = "CardNameList", menuName = "ScriptableObjects/CardNameList", order = 2)]
+public class CardNameList : ScriptableObject
+{
+    public string[] cardNames;
+    public string backfaceName;
+
+    public Sprite GetSprite(int value) {
+        if(value >= 0 && value < 52) {
+            return AtlasSpriteStore.Instance.GetSprite(cardNames[value]);
+        } else {
+            return AtlasSpriteStore.Instance.GetSprite(backfaceName);
+        }
+    }
+}
